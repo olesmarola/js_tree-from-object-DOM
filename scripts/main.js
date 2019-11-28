ec0225e2a1ba7b412aee62ee296fea8f5f676013
Oles Marola <oles_pw@ukr.net>
2019-11-28 12:59:38 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTreeDOM(element, data) {
+  element.append(createTree(data));
+}
+
+function createTree(data) {
+  if (!Object.keys(data).length) {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+
+    const branch = createTree(data[key]);
+
+    if (branch) {
+      li.append(branch);
+    }
+
+    ul.append(li);
+  }
+  return ul;
+}
+
+createTreeDOM(tree, food);
